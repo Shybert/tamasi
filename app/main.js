e2563f0b109fb751efccb51b93c398fc936973a2
Shybert <25749775+Shybert@users.noreply.github.com>
2018-03-18 10:31:38 +0100

    Renames of index related things

--- app/main.js
+++ app/main.js
@@ -2,21 +2,21 @@ const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const url = require('url');
 // Keeping a global reference of the windows
-let win;
-function createWindow() {
-    win = new BrowserWindow();
-    win.loadURL(url.format({
+let winIndex;
+function createMainWindow() {
+    winIndex = new BrowserWindow();
+    winIndex.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'file:',
         slashes: true
     }));
     // Dereference window when closed
-    win.on('closed', () => {
-        win = null;
+    winIndex.on('closed', () => {
+        winIndex = null;
     });
 }
 // Wait for Electron to be ready before loading main window
-app.on('ready', createWindow);
+app.on('ready', createMainWindow);
 app.on('window-all-closed', () => {
     // Don't quit on close if on MacOS
     if (process.platform === 'darwin') {
@@ -24,8 +24,8 @@ app.on('window-all-closed', () => {
     }
 });
 app.on('activate', () => {
-    if (win === null) {
-        createWindow();
+    if (winIndex === null) {
+        createMainWindow();
     }
 });
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -3,25 +3,25 @@ const path = require('path')
 const url = require('url')
 
 // Keeping a global reference of the windows
-let win
+let winIndex
 
-function createWindow () {
-  win = new BrowserWindow()
+function createMainWindow () {
+  winIndex = new BrowserWindow()
 
-  win.loadURL(url.format({
+  winIndex.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
   }))
 
   // Dereference window when closed
-  win.on('closed', () => {
-    win = null
+  winIndex.on('closed', () => {
+    winIndex = null
   })
 }
 
 // Wait for Electron to be ready before loading main window
-app.on('ready', createWindow)
+app.on('ready', createMainWindow)
 
 app.on('window-all-closed', () => {
   // Don't quit on close if on MacOS
@@ -31,7 +31,7 @@ app.on('window-all-closed', () => {
 })
 
 app.on('activate', () => {
-  if (win === null) {
-    createWindow()
+  if (winIndex === null) {
+    createMainWindow()
   }
 })
