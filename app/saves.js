0e8f9d7f9089404d30d52e0c3d3ce11f21fadef4
Shybert <25749775+Shybert@users.noreply.github.com>
2018-03-25 10:45:19 +0200

    Added button & overlay for creating a new save

--- app/saves.js
+++ app/saves.js
@@ -1,7 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+/* eslint-disable no-undef */
 const electron_1 = require("electron");
 // Getting the global game ID
 const id = electron_1.remote.getGlobal('sharedObject').id;
-console.log(id);
+console.log(`Global game ID: ${id}`);
+// Detecting new save button presses
+const newSaveButton = document.getElementById('newSaveButton');
+newSaveButton.addEventListener('click', () => {
+    console.log('New save button pressed');
+    const newSaveOverlay = document.getElementById('newSaveOverlay');
+    console.log('Showing the new save overlay');
+    newSaveOverlay.style.display = 'block';
+});
+// Detecting new save submits
+const createSaveButton = document.getElementById('createSaveButton');
+createSaveButton.addEventListener('click', () => {
+    console.log('New save creation');
+    const newSaveName = document.getElementById('newSaveName').value;
+    console.log(`New save name: ${newSaveName}`);
+});
 //# sourceMappingURL=saves.js.map
--- src/saves.ts
+++ src/saves.ts
@@ -1,5 +1,24 @@
+/* eslint-disable no-undef */
 import {remote} from 'electron'
 
 // Getting the global game ID
 const id: string = remote.getGlobal('sharedObject').id
-console.log(id)
+console.log(`Global game ID: ${id}`)
+
+// Detecting new save button presses
+const newSaveButton: HTMLElement = document.getElementById('newSaveButton')
+newSaveButton.addEventListener('click', (): void => {
+  console.log('New save button pressed')
+  const newSaveOverlay: HTMLElement = document.getElementById('newSaveOverlay')
+
+  console.log('Showing the new save overlay')
+  newSaveOverlay.style.display = 'block'
+})
+
+// Detecting new save submits
+const createSaveButton: HTMLElement = document.getElementById('createSaveButton')
+createSaveButton.addEventListener('click', (): void => {
+  console.log('New save creation')
+  const newSaveName: string = (document.getElementById('newSaveName') as HTMLInputElement).value
+  console.log(`New save name: ${newSaveName}`)
+})
