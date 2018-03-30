71d07a3de106d542c774c6be9d19870d39fb8ae5
Shybert <25749775+Shybert@users.noreply.github.com>
2018-03-30 11:29:56 +0200

    Removed now unnecessary functionality in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import * as path from 'path'
 import * as url from 'url'
 import * as fs from 'fs-extra'
-import {app, BrowserWindow, ipcMain} from 'electron'
+import {app, BrowserWindow} from 'electron'
 
 // Keeping a global reference of the windows
 let winMain: any
@@ -9,23 +9,27 @@ let winMain: any
 // Copy storage if it doesn't exist yet
 copyStorage()
 
-function createMainWindow () {
-  winMain = new BrowserWindow()
+async function createWindow () {
+  try {
+    winMain = new BrowserWindow()
 
-  winMain.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+    winMain.loadURL(url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+    }))
 
-  // Dereference window when closed
-  winMain.on('closed', () => {
-    winMain = null
-  })
+    // Dereference window when closed
+    winMain.on('closed', () => {
+      winMain = null
+    })
+  } catch (err) {
+    console.error(`Error while creating main window: ${err}`)
+  }
 }
 
 // Wait for Electron to be ready before loading main window
-app.on('ready', createMainWindow)
+app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
   // Don't quit on close if on MacOS
@@ -36,23 +40,7 @@ app.on('window-all-closed', () => {
 
 app.on('activate', () => {
   if (winMain === null) {
-    createMainWindow()
-  }
-})
-
-// IPC
-ipcMain.on('openSaveWindow', async (event: Event, id: string): Promise<void> => {
-  try {
-    console.log(`Loading save window with game ID '${id}'`)
-    loadWindow('saves')
-
-    // Sending the game ID
-    console.log('Setting global game ID variable')
-    global.sharedObject = {
-      id
-    }
-  } catch (err) {
-    console.log(`Error while processing openSaveWindow IPC req: ${err}`)
+    createWindow()
   }
 })
 
@@ -64,20 +52,6 @@ async function copyStorage (): Promise<void> {
     await fs.copy('app/storage', path.join(app.getPath('userData'), 'storage'), {overwrite: false})
     console.log('Copied storage/ to userData')
   } catch (err) {
-    console.log(`Error while copying storage: ${err}`)
-  }
-}
-
-// Load a new window
-async function loadWindow (name: string): Promise<void> {
-  try {
-    console.log(`Loading new window ${name}.html`)
-    winMain.loadURL(url.format({
-      pathname: path.join(__dirname, `${name}.html`),
-      protocol: 'file:',
-      slashes: true
-    }))
-  } catch (err) {
-    console.log(`Error while trying to load a new window: ${err}`)
+    console.error(`Error while copying storage: ${err}`)
   }
 }
