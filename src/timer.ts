75f23e530fe8d874c73e3353ca1991e2fda51cfa
Shybert <25749775+Shybert@users.noreply.github.com>
2018-04-15 09:48:21 +0200

    Timer now uses system clock to ensure staying in sync

--- src/timer.ts
+++ src/timer.ts
@@ -2,13 +2,17 @@
 
 class Timer {
   private interval: any
+  private currentTime: number
   private milliseconds: number = 0
 
   public switch (timeElement: HTMLElement) {
     if (!this.interval) {
       console.log('Starting timer, timer element:', timeElement)
+
+      // Get current time to calculate time difference in function
+      this.currentTime = Date.now()
       // Using arrow function to get around the setInterval 'this' problem
-      this.interval = setInterval(() => this.timer(timeElement), 1000)
+      this.interval = setInterval(() => this.timer(timeElement), 50)
     } else if (this.interval) {
       console.log('Stopping timer')
       clearInterval(this.interval)
@@ -18,10 +22,15 @@ class Timer {
   }
 
   private timer (timeElement: HTMLElement) {
-    this.milliseconds += 1
+    // Add the number of milliseconds since the last iteration using the system clock
+    // to prevent the timer getting out of sync
+    this.milliseconds += Date.now() - this.currentTime
     // Display new time on the element
     timeElement.innerHTML = `Time: ${this.milliseconds}`
     console.log('Currently elapsed time:', this.milliseconds)
+
+    // Get new currentTime
+    this.currentTime = Date.now()
   }
 
   private reset () {
