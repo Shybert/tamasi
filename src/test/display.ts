6ef523d242bcc5834e12b3ef5714edb4b7458b01
Shybert <25749775+Shybert@users.noreply.github.com>
2018-05-02 10:37:33 +0200

    Refactored addLiInfo() test

--- src/test/display.ts
+++ src/test/display.ts
@@ -16,10 +16,12 @@ describe('Display', async () => {
     })
 
     it('Should return an li element', async () => {
-      const returnedLi: HTMLElement = await display.addLiInfo('')
+      // Get node name and convert it to lowercase since nodeName returns uppercase
+      const actual: string = (await display.addLiInfo('')).nodeName.toLowerCase()
+      const expected: string = 'li'
 
       // Convert node name to lowercase since nodeName returns uppercase
-      assert.isTrue(returnedLi.nodeName.toLowerCase() === 'li', 'The returned element was not an li element')
+      assert.equal(actual, expected, 'The returned element was not an li element')
     })
     it('Should append the provided text to the li', async () => {
       const returnedLi: HTMLElement = await display.addLiInfo(text)
@@ -29,7 +31,8 @@ describe('Display', async () => {
     it('Should not add a class or an ID if they are not provided', async () => {
       const returnedLi: HTMLElement = await display.addLiInfo('')
 
-      assert.isTrue(returnedLi.classList.length === 0 && returnedLi.hasAttribute('id') === false, "The returned li contained a class or an ID, even though it wasn't provided")
+      assert.isTrue(returnedLi.classList.length === 0, 'The returned li had a class when no class was provided')
+      assert.isFalse(returnedLi.hasAttribute('id'), 'The returned li had an ID when no ID was provided')
     })
     it('Should add the provided class to the li', async () => {
       const returnedLi: HTMLElement = await display.addLiInfo('', {theClass: exampleClass})
@@ -44,7 +47,8 @@ describe('Display', async () => {
     it('Should add both an ID and a class if both are provided', async () => {
       const returnedLi: HTMLElement = await display.addLiInfo('', {id: id, theClass: exampleClass})
 
-      assert.isTrue(returnedLi.classList.contains(exampleClass) && returnedLi.id === id, 'The returned li did not contain the ID or the class provided')
+      assert.isTrue(returnedLi.classList.contains(exampleClass), 'The returned li did not have the provided class')
+      assert.equal(returnedLi.id, id, 'The returned li did not have the provided ID')
     })
   })
 
