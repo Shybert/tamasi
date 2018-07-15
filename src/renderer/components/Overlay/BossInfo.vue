37d2f701f9a7da4a057313db5e1311227b9f2c34
Shybert <25749775+Shybert@users.noreply.github.com>
2018-07-15 11:14:41 +0200

    Now displaying what each value in the displayed boss list is for each boss

--- src/renderer/components/Overlay/BossInfo.vue
+++ src/renderer/components/Overlay/BossInfo.vue
@@ -1,8 +1,8 @@
 <template>
   <ul>
-    <li id="bossName">{{bossInfo.name}}</li>
-    <li id="bossTime">{{bossInfo.time}}</li>
-    <li id="bossDeaths">{{bossInfo.deaths}}</li>
+    <li id="bossName">Name: {{bossInfo.name}}</li>
+    <li id="bossTime">Time: {{bossInfo.time}}</li>
+    <li id="bossDeaths">Deaths: {{bossInfo.deaths}}</li>
   </ul>
 </template>
 
