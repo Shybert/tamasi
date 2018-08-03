d7acad1e908bcb0e2765b128f9c77653d84f20d2
Shybert <25749775+Shybert@users.noreply.github.com>
2018-08-03 20:53:56 +0200

    Refactored Saves.vue with vue-class-component and added tests

--- src/renderer/components/Saves/Saves.vue
+++ src/renderer/components/Saves/Saves.vue
@@ -9,31 +9,30 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import {Component} from 'vue-property-decorator'
 import * as saves from '../../storage/saves'
 
 import SaveListComponent from './SaveList.vue'
 import NewSaveOverlayComponent from './NewSaveOverlay.vue'
 
-export default Vue.extend({
-  data () {
-    return {
-      saveList: saves.getSaves(this.$route.params.gameId),
-      showNewSaveOverlay: false
-    }
-  },
-  methods: {
-    openNewSaveOverlay () {
-      this.$data.showNewSaveOverlay = true
-    },
-    createNewSave (saveName: string) {
-      saves.createSave(this.$route.params.gameId, saveName)
+@Component({components: {SaveListComponent, NewSaveOverlayComponent}})
+export default class Saves extends Vue {
+  showNewSaveOverlay: boolean = false
+  get saveList () {
+    return saves.getSaves(this.$route.params.gameId)
+  }
 
-      // Redisplay the save list now that a new save has been created
-      this.$data.saveList = saves.getSaves(this.$route.params.gameId)
-      this.$data.showNewSaveOverlay = false
-    }
-  },
-  components: {SaveListComponent, NewSaveOverlayComponent}
-})
+  openNewSaveOverlay (): void {
+    this.$data.showNewSaveOverlay = true
+  }
+
+  createNewSave (saveName: string): void {
+    saves.createSave(this.$route.params.gameId, saveName)
+
+    // Redisplay the save list now that a new save has been created
+    this.$data.saveList = saves.getSaves(this.$route.params.gameId)
+    this.$data.showNewSaveOverlay = false
+  }
+}
 </script>
 
---
+++ test/components/Saves/Saves.test.ts
@@ -0,0 +1,92 @@
+import {shallowMount} from '@vue/test-utils'
+import Saves from '../../../src/renderer/components/Saves/Saves.vue'
+import SaveList from '../../../src/renderer/components/Saves/SaveList.vue'
+import NewSaveOverlay from '../../../src/renderer/components/Saves/NewSaveOverlay.vue'
+import * as saves from '../../../src/renderer/storage/saves'
+
+jest.mock('../../../src/renderer/storage/saves', () => ({
+  getSaves: jest.fn(),
+  createSave: jest.fn()
+}))
+
+function factory () {
+  return shallowMount(Saves, {
+    mocks: {
+      $route: {
+        params: {gameId: 'testGameId'}
+      }
+    }
+  })
+}
+
+describe('Saves.vue', () => {
+  test('Renders the SaveList component', () => {
+    const wrapper = factory()
+    expect(wrapper.contains(SaveList)).toBe(true)
+  })
+
+  test('Fetches list of saves with the gameID as an arg', () => {
+    factory()
+    expect(saves.getSaves).toBeCalledWith('testGameId')
+  })
+
+  test("Calls createNewSave() method when 'createNewSave' is emitted on the NewSaveOverlay component", () => {
+    const wrapper = factory()
+    // Make sure the new save overlay is rendered
+    wrapper.setData({showNewSaveOverlay: true})
+    expect(wrapper.contains(NewSaveOverlay)).toBe(true)
+
+    wrapper.setMethods({createNewSave: jest.fn()})
+    wrapper.find(NewSaveOverlay).vm.$emit('createNewSave')
+
+    expect(wrapper.vm.createNewSave).toBeCalled()
+  })
+
+  describe('New Save Overlay', () => {
+    test('Not displayed by default', () => {
+      const wrapper = factory()
+      expect(wrapper.contains(NewSaveOverlay)).toBe(false)
+    })
+
+    test("Has a button with the text 'Open New Save Overlay'", () => {
+      const wrapper = factory()
+      const button = wrapper.find('button')
+
+      expect(button.text()).toBe('Open New Save Overlay')
+    })
+
+    test("Clicking the 'Open New Save Overlay' button displays the NewSaveOverlay component", () => {
+      const wrapper = factory()
+      const button = wrapper.find('button')
+      button.trigger('click')
+
+      expect(wrapper.contains(NewSaveOverlay)).toBe(true)
+    })
+  })
+
+  describe('createNewSave()', () => {
+    test('Calls saves.createSave() with the game ID and save name as args', () => {
+      const wrapper = factory()
+      wrapper.vm.createNewSave('testSaveName')
+
+      expect(saves.createSave).toBeCalledWith('testGameId', 'testSaveName')
+    })
+
+    test('Fetches a new list of saves after the save has been created', () => {
+      const wrapper = factory()
+      wrapper.vm.createNewSave('testSaveName')
+
+      expect(saves.getSaves).toBeCalled()
+    })
+
+    test('Hides the NewSaveOverlay', () => {
+      const wrapper = factory()
+      // Make sure the new save overlay is rendered
+      wrapper.setData({showNewSaveOverlay: true})
+      expect(wrapper.contains(NewSaveOverlay)).toBe(true)
+
+      wrapper.vm.createNewSave('testSaveName')
+      expect(wrapper.contains(NewSaveOverlay)).toBe(false)
+    })
+  })
+})
