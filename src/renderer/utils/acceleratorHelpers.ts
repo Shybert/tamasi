b8e7946c23c9f19ea0b02580acd51486a91d1bef
Shybert <25749775+Shybert@users.noreply.github.com>
2018-10-24 13:45:32 +0200

    acceleratorHelpers.ts: No longer exporting keys/modifiers regexes, added isModifier() and isValidKey(), and fixed name of test file

--- src/renderer/utils/acceleratorHelpers.ts
+++ src/renderer/utils/acceleratorHelpers.ts
@@ -1,5 +1,14 @@
-export const modifiersRegex: RegExp = /^Command|Cmd|Control|Ctrl|CommandOrControl|CmdOrCtrl|Alt|Option|AltGr|Shift|Super$/
-export const keysRegex: RegExp = /^([a-zA-Z0-9,_:;*?=(){}$&%#"!@^~|'`<>\.\-\[\]\/\\]|F1?([1-9]|10|2[0-4])|Plus|Space|Tab|Backspace|Delete|Insert|Return|Enter|Up|Down|Left|Right|Home|End|PageUp|PageDown|Escape|Esc|VolumeUp|VolumeDown|VolumeMute|MediaNextTrack|MediaPreviousTrack|MediaStop|MediaPlayPause|PrintScreen)$/
+export function isModifier (key: string): boolean {
+  const modifiersRegex: RegExp = /^Command|Cmd|Control|Ctrl|CommandOrControl|CmdOrCtrl|Alt|Option|AltGr|Shift|Super$/
+  if (modifiersRegex.test(key)) return true
+  return false
+}
+
+export function isValidKey (key: string): boolean {
+  const keysRegex: RegExp = /^([a-zA-Z0-9,_:;*?=(){}$&%#"!@^~|'`<>\.\-\[\]\/\\]|F1?([1-9]|10|2[0-4])|Plus|Space|Tab|Backspace|Delete|Insert|Return|Enter|Up|Down|Left|Right|Home|End|PageUp|PageDown|Escape|Esc|VolumeUp|VolumeDown|VolumeMute|MediaNextTrack|MediaPreviousTrack|MediaStop|MediaPlayPause|PrintScreen)$/
+  if (keysRegex.test(key)) return true
+  return false
+}
 
 export function isValidKeybind (str: string): boolean {
   const parts: string[] = str.split('+')
@@ -7,8 +16,8 @@ export function isValidKeybind (str: string): boolean {
 
   let hasKey: boolean = false
   const partsValid: boolean = parts.every(part => {
-    if (modifiersRegex.test(part) && !modifiers.includes(part)) return true
-    if (keysRegex.test(part)) {
+    if (isModifier(part) && !modifiers.includes(part)) return true
+    if (isValidKey(part)) {
       if (hasKey) return false // There can only be one key
       hasKey = true
       return true
