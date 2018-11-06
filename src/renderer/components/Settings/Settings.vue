ef172647afebde209cfb76bac897e9eb684bb228
Shybert <25749775+Shybert@users.noreply.github.com>
2018-11-06 17:34:19 +0100

    Moved Categories.vue into Settings.vue

--- src/renderer/components/Settings/Settings.vue
+++ src/renderer/components/Settings/Settings.vue
@@ -3,17 +3,29 @@
     <h1>Settings</h1>
     <SettingsNavComponent></SettingsNavComponent>
 
-    <CategoriesComponent></CategoriesComponent>
+    <div id="categories">
+      <div class="category" v-for="(category, categoryId) in settings" :key="categoryId" :id="categoryId">
+        <h2 class="categoryLabel">{{category.label}}</h2>
+        <p class="categoryDesc">{{category.description}}</p>
+
+        <SettingComponent v-for="(settingInfo, settingId) in category.settings" :key="settingId" :settingId="settingId" :categoryId="categoryId"></SettingComponent>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import {Component} from 'vue-property-decorator'
+import {ISettingsCategories} from '../../store/settingsData'
 
 import SettingsNavComponent from './SettingsNav.vue'
-import CategoriesComponent from './Categories.vue'
+import SettingComponent from './Setting.vue'
 
-@Component({components: {SettingsNavComponent, CategoriesComponent}})
-export default class Settings extends Vue {}
+@Component({components: {SettingsNavComponent, SettingComponent}})
+export default class Settings extends Vue {
+    get settings (): ISettingsCategories {
+    return this.$store.state.settings.defaultSettings
+  }
+}
 </script>
