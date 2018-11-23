4b85e617c34657c7f380c44485386211f29dab15
Shybert <25749775+Shybert@users.noreply.github.com>
2018-11-23 08:36:24 +0100

    Preventing creation of save if no name is inputted

--- src/renderer/components/Saves/NewSaveOverlay.vue
+++ src/renderer/components/Saves/NewSaveOverlay.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="newSaveOverlay">
     <h2>New Save Overlay</h2>
-    <input id="newSaveName" v-model="newSaveName" placeholder="New Save Name">
+
+    <div class="inputError" v-if="inputError">{{inputError}}</div>
+    <input id="newSaveName" v-model="newSaveName" placeholder="New Save Name" required>
     <button @click="createSave" id="createSave">Create New Save</button>
+
     <button @click="closeOverlay" id="closeOverlay">Close Overlay</button>
   </div>
 </template>
@@ -15,8 +18,15 @@ import {Component, Prop} from 'vue-property-decorator'
 export default class NewSaveOverlay extends Vue {
   @Prop({type: String, required: true}) gameId!: string
   newSaveName: string = ''
+  inputError: string | null = null
 
   createSave (): void {
+    this.inputError = null
+    if (!this.newSaveName) {
+      this.inputError = 'A name for the save is required.'
+      return
+    }
+
     this.$store.dispatch('createSave', {gameId: this.gameId, saveName: this.newSaveName})
   }
 
@@ -26,3 +36,8 @@ export default class NewSaveOverlay extends Vue {
 }
 </script>
 
+<style scoped>
+.inputError {
+  color: red;
+}
+</style>
