922613a9d537b5bc2cbe2a99c9f18c00a0ffbf5b
Shybert <25749775+Shybert@users.noreply.github.com>
2019-02-16 11:12:16 +0100

    Refactored Setting class * Class is now abstract * isValidSettingValue() is now abstract * Turned the settingValue property into a getter/setter, and moved validating the user setting value out of the constructor and into the getter/setter, so as not to call a function that can be changed in a derived class inside the constructor

--- src/renderer/settings/settingsService.ts
+++ src/renderer/settings/settingsService.ts
@@ -1,21 +1,17 @@
 import Store from 'electron-store'
 const userSettings = new Store({name: 'userSettings', cwd: 'storage'})
 
-class Setting<T> {
+abstract class BaseSetting<T> {
   private id: string
   public label: string
   public description: string
-  public settingValue: T
+  private userSettingValue: unknown
   private defaultValue: T
 
   private getUserSetting (): unknown {
     const userSetting: unknown = userSettings.get(this.id)
     if (userSetting) return userSetting
     return null
-  }
-
-  private isValidSettingValue (value: any): boolean {
-    return true
   }
 
   constructor (id: string, label: string, description: string, defaultValue: T) {
@@ -25,20 +21,28 @@ class Setting<T> {
     this.defaultValue = defaultValue
 
     const userSetting = this.getUserSetting()
-    if (userSetting && this.isValidSettingValue(userSetting)) this.settingValue = userSetting as T
-    else this.settingValue = defaultValue
+    if (userSetting) this.userSettingValue = userSetting
+  }
+
+  get settingValue (): T {
+    return this.isValidSettingValue(this.userSettingValue) ? this.userSettingValue as T : this.defaultValue
+  }
+  set settingValue (value: T) {
+    this.userSettingValue = value
+    userSettings.set(this.id, value)
   }
 
   public isSettingValueDefault (): boolean {
     return this.settingValue === this.defaultValue
   }
 
-  public setSettingValue (value: any): void {
-    if (this.isValidSettingValue(value)) {
-      this.settingValue = value
-      userSettings.set(this.id, value)
+  abstract isValidSettingValue (value: any): boolean
     }
   }
 }
 
-export class SettingKeybind extends Setting<number[]> {}
+export class SettingKeybind extends BaseSetting<number[]> {
+  isValidSettingValue (value: any): boolean {
+    return true
+  }
+}
