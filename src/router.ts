94d5143f4bef0abc28c405cc99149942d0846312
Shybert <25749775+Shybert@users.noreply.github.com>
2020-01-05 08:20:02 +0100

    Switched back to Typescript 3.7
    Added a temporary workaround for the vue-router / composition API bug

--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,20 @@
-import Vue from 'vue'
+import Vue, { VueConstructor } from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/Home.vue'
 import Overlay from '@/views/Overlay.vue'
 
 Vue.use(Router)
 
+// * as VueConstructor<Vue> is a temporary workaround
+// because of a bug in the composition API.
+// https://github.com/vuejs/vue-router/issues/3019
 const routes = [
-  { path: '/', component: Home, props: true },
-  { path: '/overlay/:saveId', component: Overlay, props: true }
+  { path: '/', component: Home as VueConstructor<Vue>, props: true },
+  {
+    path: '/overlay/:saveId',
+    component: Overlay as VueConstructor<Vue>,
+    props: true
+  }
 ]
 
 export default new Router({ routes })
