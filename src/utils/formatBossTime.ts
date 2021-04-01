b4f6b6fa51f27aafce10d9affad9d0c798e939f5
Shybert <25749775+Shybert@users.noreply.github.com>
2021-04-01 14:27:59 +0200

    Improved formatting boss time

--- src/utils/formatBossTime.ts
+++ src/utils/formatBossTime.ts
@@ -1,20 +1,15 @@
-export default function formatBossTime(
-  timeInMilliseconds: number,
-  includeMilliseconds = true
-) {
-  const date: Date = new Date(timeInMilliseconds)
+export default function formatBossTime(timeInMilliseconds: number) {
+  const date = new Date(timeInMilliseconds)
 
-  // Convert date to relevant time, then pad it with leading zeroes
   const hours = Math.floor(timeInMilliseconds / 3600000)
-    .toString()
-    .padStart(2, '0')
-  const minutes = date.getUTCMinutes().toString().padStart(2, '0')
-  const seconds = date.getUTCSeconds().toString().padStart(2, '0')
+  const hoursString = hours ? `${hours}h` : ''
+
+  const minutes = date.getUTCMinutes()
+  const minutesString = minutes ? `${minutes}m` : ''
+
+  const seconds = date.getUTCSeconds()
   const milliseconds = date.getUTCMilliseconds().toString().padStart(3, '0')
+  const secondsString = `${seconds}.${milliseconds}s`
 
-  const formattedTime = `${hours}:${minutes}:${seconds}${
-    includeMilliseconds ? `.${milliseconds}` : ''
-  }`
-
-  return formattedTime
+  return [hoursString, minutesString, secondsString].join(' ')
 }
